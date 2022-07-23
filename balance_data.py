2638e9df12db6f3846fe6a72ce1173b95687ab42
Pratheeth-Adiga <83741965+Pratheeth-Adiga@users.noreply.github.com>
2022-07-23 17:45:31 +0530

    uploading finished files

---
+++ balance_data.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+from collections import Counter
+from random import shuffle
+
+train_data = np.load('training_data.npy',allow_pickle=True)
+
+df = pd.DataFrame(train_data)
+print(df.head())
+print(Counter(df[1].apply(str)))
+
+lefts = []
+rights = []
+forwards = []
+backwards = []
+
+shuffle(train_data)
+
+for data in train_data:
+    img = data[0]
+    choice = data[1]
+
+    if choice == [1,0,0,0]:
+        lefts.append([img,choice])
+    elif choice == [0,1,0,0]:
+        forwards.append([img,choice])
+    elif choice == [0,0,1,0]:
+        rights.append([img,choice])
+    elif choice == [0,0,0,1]:
+        backwards.append([img,choice])
+    else:
+        print('no match')
+
+
+forwards = forwards[:len(lefts)][:len(rights)][:len(backwards)]
+lefts = lefts[:len(forwards)]
+rights = rights[:len(forwards)]
+backwards = backwards[:len(lefts)][:len(rights)]
+
+
+final_data = forwards + lefts + rights + backwards
+shuffle(final_data)
+
+np.save('training_data-1-balanced.npy', final_data)
+
+
+
+
---
+++ test_model.py
@@ -0,0 +1,107 @@
+import numpy as np
+from grabscreen import grab_screen
+import cv2
+import time
+from directkeys import PressKey,ReleaseKey, W, A, S, D
+from alexnet import alexnet
+from getkeys import key_check
+
+import random
+
+WIDTH = 160
+HEIGHT = 120
+LR = 1e-3
+EPOCHS = 10
+MODEL_NAME = 'pygta5-car-fast-{}-{}-{}-epochs-300K-data.model'.format(LR, 'alexnetv2',EPOCHS)
+
+t_time = 0.09
+
+def straight():
+    PressKey(W)
+    ReleaseKey(A)
+    ReleaseKey(D)
+
+def left():
+    PressKey(W)
+    PressKey(A)
+    #ReleaseKey(W)
+    ReleaseKey(D)
+    #ReleaseKey(A)
+    time.sleep(t_time)
+    ReleaseKey(A)
+
+def right():
+    PressKey(W)
+    PressKey(D)
+    ReleaseKey(A)
+    #ReleaseKey(W)
+    #ReleaseKey(D)
+    time.sleep(t_time)
+    ReleaseKey(D)
+
+def reverse():
+    PressKey(S)
+    ReleaseKey(W)
+    
+model = alexnet(WIDTH, HEIGHT, LR)
+model.load(MODEL_NAME)
+
+def main():
+    last_time = time.time()
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        time.sleep(1)
+
+    paused = False
+    while(True):
+        
+        if not paused:
+            screen = grab_screen(region=(0,40,800,640))
+            print('loop took {} seconds'.format(time.time()-last_time))
+            last_time = time.time()
+            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+            screen = cv2.resize(screen, (160,120))
+
+            prediction = model.predict([screen.reshape(160,120,1)])[0]
+            print(prediction)
+
+            turn_thresh = .52
+            fwd_thresh = 0.75
+            rvr_thresh = .12
+
+            if prediction[1] > fwd_thresh:
+                straight()
+            elif prediction[0] > turn_thresh:
+                left()
+            elif prediction[2] > turn_thresh:
+                right()
+            elif prediction[3] > rvr_thresh:
+                reverse()
+            else:
+                straight()
+
+        keys = key_check()
+
+        if 'T' in keys:
+            if paused:
+                paused = False
+                time.sleep(1)
+            else:
+                paused = True
+                ReleaseKey(A)
+                ReleaseKey(W)
+                ReleaseKey(D)
+                ReleaseKey(S)
+                time.sleep(1)
+
+main()       
+
+
+
+
+
+
+
+
+
+
